a4701dd1be68784922f472e620095d857d8f98a5
Madeleine Saucier <msaucier2020@gmail.com>
2023-02-16 13:01:04 -0500

    Take while loop out of server

--- project1_server.py
+++ project1_server.py
@@ -14,37 +14,36 @@ server_str = "Server of Madeleine Saucier"
 serverSocket.listen(1) 
 print("Server ready to receive")
 
+connectionSocket, addr = serverSocket.accept()
 
-while True:
-    connectionSocket, addr = serverSocket.accept()
+#receive and decode client message
+client_message = connectionSocket.recv(5010).decode()
+print("Client message received")
 
-    #receive and decode client message
-    client_message = connectionSocket.recv(5010).decode()
-    print("Client message received")
+#get client name from message
+client_name = ''.join([i for i in client_message if not i.isdigit()])
 
-    #get client name from message
-    client_name = ''.join([i for i in client_message if not i.isdigit()])
-
-    #get integer from message
-    for x in client_message.split():
-        if(x.isdigit()):
-            client_int = int(x)
+#get integer from message
+for x in client_message.split():
+    if(x.isdigit()):
+        client_int = int(x)
     
-    #get random integer and compute sum
-    server_int = random.randint(0, 100)
-    sum = server_int + client_int
+#get random integer and compute sum
+server_int = random.randint(0, 100)
+sum = server_int + client_int
 
-    #print all info
-    print(client_name)
-    print(server_str)
-    print("Client int: " + str(client_int))
-    print("Server int: " + str(server_int))
-    print("Sum: " + str(sum))
+#print all info
+print(client_name)
+print(server_str)
+print("Client int: " + str(client_int))
+print("Server int: " + str(server_int))
+print("Sum: " + str(sum))
 
-    #create server message, encode and send to client
-    server_message = server_str + " " + str(server_int)
-    connectionSocket.send(server_message.encode())
-    print("Message sent to client")
+#create server message, encode and send to client
+server_message = server_str + " " + str(server_int)
+connectionSocket.send(server_message.encode())
+print("Message sent to client")
 
-    connectionSocket.close()
-    print("Server socket closed")
+connectionSocket.close()
+serverSocket.close()
+print("Server socket closed")
